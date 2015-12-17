40b5b3f0a6e25420835f8c6033f19166132b6715
kay@hiktechnology.com <MOM@dad24>
2015-12-17 15:43:43 +0530

    commit by vasanth

--- app/src/main/java/com/hik/trendycraftshow/PaymentMethod.java
+++ app/src/main/java/com/hik/trendycraftshow/PaymentMethod.java
@@ -1,8 +1,12 @@
 package com.hik.trendycraftshow;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.hik.trendycraftshow.Utils.IsTablet;
@@ -11,11 +15,28 @@ public class PaymentMethod extends NavigationDrawer {
 
     IsTablet tablet;
     boolean isTablet;
+    ListView payment_list;
+    EditText paypal_emailid;
+    Button add_emailid,addpaypal;
+    LinearLayout add_layout,addemail_layout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         title=(TextView)findViewById(R.id.titletoolbar);
+
+        payment_list=(ListView)findViewById(R.id.paymentmethod_listview);
+        addpaypal=(Button)findViewById(R.id.add);
+        paypal_emailid=(EditText)findViewById(R.id.paypal_emailid);
+        add_emailid=(Button)findViewById(R.id.addpaypal_emailid);
+        add_layout=(LinearLayout)findViewById(R.id.add_layout);
+        addemail_layout=(LinearLayout)findViewById(R.id.addemail_layout);
+
+
+
+
         title.setText("PAYMENT METHOD");
         isTablet=tablet.isTablet(getApplicationContext());
         isTablet = tablet.isTablet(getApplicationContext());
@@ -24,6 +45,27 @@ public class PaymentMethod extends NavigationDrawer {
         } else {
             getLayoutInflater().inflate(R.layout.activity_payment_method_mob, container);
         }
+
+        addpaypal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addemail_layout.setVisibility(View.VISIBLE);
+                add_layout.setVisibility(View.GONE);
+            }
+        });
+
+        add_emailid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                addemail_layout.setVisibility(View.GONE);
+                add_layout.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
+
     }
 
 }
